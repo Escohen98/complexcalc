86dc490938148e9659007859138102280e2f19e2
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-01-16 16:46:00 -0800

    Finished basic operations functions.

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -11,4 +11,37 @@ import Foundation
 // All your work will go in here
 class Calculator {
     
+    //Adds lhs with rhs. Returns sum.
+    //lhs (Int) - left argument to be added.
+    //rhs (Int) - right argument to be added.
+    func add(lhs : Int, rhs : Int) -> Int {
+        return lhs + rhs;
+    }
+    
+    //Subtracts lhs by rhs. Returns difference.
+    //lhs (Int) - left argument to be subtracted from.
+    //rhs (Int) - right argument to be used to subtract.
+    func subtract(lhs : Int, rhs : Int) -> Int {
+        return lhs - rhs;
+    }
+    
+    //Multiplies lhs with rhs. Returns product.
+    //lhs (Int) - left argument to be multiplied.
+    //rhs (Int) - right argument to be multiplied.
+    func multiply(lhs : Int, rhs : Int) -> Int {
+        return lhs * rhs;
+    }
+    
+    //Divied lhs by rhs. Returns quotient or 0 if rhs is 0.
+    //lhs (Int) - left argument to be divided from.
+    //rhs (Int) - right argument to be used to divide.
+    func divide(lhs : Int, rhs : Int) -> Int {
+        if(rhs != 0) {
+            return lhs/rhs
+        }
+        
+        return 0;
+    }
+    
+    
 }
