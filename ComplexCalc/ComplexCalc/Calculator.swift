85bcd9301d363d04177fc46cb1b4cd6c5b708f70
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-01-16 17:06:39 -0800

    Finished avg and count functions. Starting on mathOp. Also trying to figure out how to handle ints, tuples, and arrays for simple ops.

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -11,31 +11,39 @@ import Foundation
 // All your work will go in here
 class Calculator {
     
-    //Adds lhs with rhs. Returns sum.
-    //lhs (Int) - left argument to be added.
-    //rhs (Int) - right argument to be added.
-    func add(lhs : Int, rhs : Int) -> Int {
+    /*
+    * Adds lhs with rhs. Returns sum.
+    * lhs (Int) - left argument to be added.
+    * rhs (Int) - right argument to be added.
+    */
+    func add(_ lhs : (Int),_ rhs : (Int)) -> (Int) {
         return lhs + rhs;
     }
     
-    //Subtracts lhs by rhs. Returns difference.
-    //lhs (Int) - left argument to be subtracted from.
-    //rhs (Int) - right argument to be used to subtract.
-    func subtract(lhs : Int, rhs : Int) -> Int {
+    /*
+    * Subtracts lhs by rhs. Returns difference.
+    * lhs (Int) - left argument to be subtracted from.
+    * rhs (Int) - right argument to be used to subtract.
+    */
+    func subtract(_ lhs : (Int),_ rhs : (Int)) -> (Int) {
         return lhs - rhs;
     }
     
-    //Multiplies lhs with rhs. Returns product.
-    //lhs (Int) - left argument to be multiplied.
-    //rhs (Int) - right argument to be multiplied.
-    func multiply(lhs : Int, rhs : Int) -> Int {
+    /*
+    * Multiplies lhs with rhs. Returns product.
+    * lhs (Int) - left argument to be multiplied.
+    * rhs (Int) - right argument to be multiplied.
+    */
+    func multiply(_ lhs : (Int),_ rhs : (Int)) -> (Int) {
         return lhs * rhs;
     }
     
-    //Divied lhs by rhs. Returns quotient or 0 if rhs is 0.
-    //lhs (Int) - left argument to be divided from.
-    //rhs (Int) - right argument to be used to divide.
-    func divide(lhs : Int, rhs : Int) -> Int {
+    /*
+    * Divides lhs by rhs. Returns quotient or 0 if rhs is 0.
+    * lhs (Int) - left argument to be divided from.
+    * rhs (Int) - right argument to be used to divide.
+    */
+    func divide(_ lhs : (Int),_ rhs : (Int)) -> (Int) {
         if(rhs != 0) {
             return lhs/rhs
         }
@@ -43,5 +51,29 @@ class Calculator {
         return 0;
     }
     
+    func mathOp(_ args : [Int], beg : Int; _ op: (Int)) -> Int {
+        return 1
+    }
+    
+    /*
+    * Returns the number of elements in args.
+    * args [array] - array of integers to be counted
+    */
+    func count(_ args : [Int]) -> Int {
+         return args.count;
+    }
+    
+    /*
+    * Adds all integers in args, then divides by the count. Returns result.
+    * args [array] - array of integers
+    */
+    func avg(_ args : [Int]) -> Int {
+        var sum = 0
+        for i in args {
+            sum += i
+        }
+        
+        return sum / count(args)
+    }
     
 }
